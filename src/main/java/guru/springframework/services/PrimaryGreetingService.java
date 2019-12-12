8236b99900eb3e42337ac799541068ac9b484d1f
milagroscabrera <milagroscabrera@192.168.1.150>
2019-12-11 23:20:31 -0500

    fixed

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,11 @@ public class PrimaryGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
 
+    @Autowired
+    public void setGreetingRepository(GreetingRepository greetingRepository) {
+    	this.greetingRepository = greetingRepository;
+    }
+    
     @Override
     public String sayGreeting() {
         return greetingRepository.getEnglishGreeting();
